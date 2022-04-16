fbb38e2e438c14136529f0a9ae0d3124381e469c
Niklas Arnitz <niklas.arnitz@icloud.com>
2022-04-16 14:26:22 +0200

    Updated Build Script

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build . -t niklasarnitz/churchtools-organigram
+docker build . --no-cache -t niklasarnitz/churchtools-organigram:latest
 docker push niklasarnitz/churchtools-organigram:latest
